92f64295c93290668b8ba2c0ca53553d3b38c21f
ADMIN <admin@ADMINs-MacBook-Pro.local>
2018-10-24 21:12:05 +0700

    youtubeHAT

---
+++ HAT/HAT.js
@@ -0,0 +1,22 @@
+var arrayTubes = new Array(
+    {
+        id: 'UCXao7aTDQM',
+        title: 'Tháng Tư Là Lời Nói Dối Của Em',
+        author: 'Hà Anh Tuấn'
+    },
+    {
+        id: 'dns2WLu8Su8',
+        title: 'Người Tình Mùa Đông',
+        author: 'Hà Anh Tuấn'
+    },
+    {
+        id: 'XyjhXzsVdiI',
+        title: 'Tình Thôi Xót Xa',
+        author: 'Hà Anh Tuấn'
+    },
+    {
+        id: 'nrpjNgZCdlM',
+        title: 'Tái Bút Anh Yêu Em',
+        author: 'Hà Anh Tuấn'
+    }
+);
